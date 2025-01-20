ea1b0148844c09caee65edf2696bf32116d9deff
Matthew McQuaid <matthew.mcquaid@gmail.com>
2025-01-20 17:37:34 -0500

    cleanup

--- client/src/components/DogsList.js
+++ client/src/components/DogsList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import CustomButton from './CustomButton.js';
 import Dog from './Dog.js';
 
-const DogList = ({ data, breeds , dogBreedNam, handleDelete,dogBreedName }) => {
+const DogList = ({ data, breeds, handleDelete, dogBreedName }) => {
     return (
         <>
-        <h2>Fetched Data:</h2>
-        <ul>
-            {data ? data.map((item) => (
-            <li key={item.id} className='Dog-List'>
-                <Dog dog={item} breedName={dogBreedName(item.breed_id, breeds)} />
-                <CustomButton onClick={e => handleDelete(item.id)}>DELETE</CustomButton>
-            </li>
-            )):
-            <div>No Data</div>}
-        </ul>
+            <h2>Fetched Data:</h2>
+            <ul>
+                {data ? data.map((item) => (
+                <li key={item.id} className='Dog-List'>
+                    <Dog dog={item} breedName={dogBreedName(item.breed_id, breeds)} />
+                    <CustomButton onClick={e => handleDelete(item.id)}>DELETE</CustomButton>
+                </li>
+                )):
+                <div>No Data</div>}
+            </ul>
         </>
     );
 }
